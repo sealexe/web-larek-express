36fd7d8ffe24a0dc2c6ce82f57b245664d456746
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-06-05 18:10:41 +0300

    созданы роуты и контроллеры для товаров

---
+++ backend/src/controllers/products.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import Product from '../models/product';
+
+export const getProducts = (_req: Request, res: Response) => {
+  Product.find({})
+    .then((products) => res.send({ data: products }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+export const createProduct = (req: Request, res: Response) => {
+  const {
+    title, image, category, description, price,
+  } = req.body;
+  Product.create({
+    title, image, category, description, price,
+  })
+    .then((product) => res.send({ data: product }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
