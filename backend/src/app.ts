73b0b553fa453233c20b524fe44c3c570748f3af
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-06-13 18:23:17 +0300

    добавлен файл конфига

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,17 +5,18 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import path from 'path';
 import { errors } from 'celebrate';
+import { DB_ADDRESS, PORT } from './config';
 import errorHandler from './middlewares/error-handler';
 import productsRouter from './routes/products';
 import orderRouter from './routes/order';
 import BadRequestError from './errors/bad-request-error';
-import { requestLogger, errorLogger } from './middlewares/logger';
+import { requestLogger, errorLogger, logger } from './middlewares/logger';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
+mongoose.connect(DB_ADDRESS);
 
 app.use(requestLogger);
 
@@ -28,6 +29,6 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('listening at port 3000');
+app.listen(PORT, () => {
+  logger.info('listening at port 3000');
 });
--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -16,3 +16,8 @@ export const errorLogger = expressWinston.errorLogger({
   ],
   format: winston.format.json(),
 });
+
+export const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.simple(),
+});
